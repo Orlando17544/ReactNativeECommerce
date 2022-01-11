01ec97ceb31063d4de30550eded0f999929f82b7
Orlando Canchola <orcj17544@gmail.com>
2022-01-11 13:27:01 -0600

    CountrySexScreen made

--- src/screens/CountrySexScreen.js
+++ src/screens/CountrySexScreen.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -15,13 +15,44 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
+  TouchableOpacity,
+  Dimensions
 } from 'react-native';
 
+import Flag from 'react-native-flags';
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
 const CountrySexScreen = () => {
-  //SplashScreen.hide();
+  const [countryData, setCountryData] = useState({name: 'Mexico', code: 'MX'});
+  const [currency, setCurrency] = useState('USD');
+
 
   return (
-	  <View>
+	  <View style={{flex: 1, alignItems: 'center', backgroundColor: '#FFFFFF'}}>
+	  	<Image 
+	  	source={require('../../android/app/src/main/res/drawable-mdpi/launch_screen.png')} 
+		/>
+	  	<Text style={{color: '#000000', fontWeight: 'bold'}}>DELIVER TO:</Text>
+	  	<View style={{alignItems: 'center', flexDirection: 'row'}}>
+	  		<Flag
+	  			code={countryData.code}
+	  			size={32}	
+	  			type='flat'
+	  		/>
+	  		<Text style={{color: '#000000'}}> {countryData.name}, $ {currency}</Text>
+	  		<Text style={{color: '#000000', fontWeight: 'bold'}}>    |    CHANGE</Text>
+	  	</View>
+	  	<View style={{flex: 1, justifyContent: 'space-evenly'}}>
+	  		<TouchableOpacity style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#2d2d2d', width: screenWidth * 0.85, height: screenHeight * 0.08}}>
+	  			<Text style={{color: '#ffffff', fontSize: 15, fontWeight: 'bold'}}>WOMEN</Text>
+	  		</TouchableOpacity>
+	  		<TouchableOpacity style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#2d2d2d', width: screenWidth * 0.85, height: screenHeight * 0.08}}>
+	  			<Text style={{color: '#ffffff', fontSize: 15, fontWeight: 'bold'}}>MEN</Text>
+	  		</TouchableOpacity>
+	  	</View>
 	  </View>
   );
 };
