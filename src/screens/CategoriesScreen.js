419a2d38da8d7a7c5f8aed6df97b0395cf0d5dd1
Orlando Canchola Jimenez <orcj17544@gmail.com>
2022-02-04 15:39:37 -0600

    NewInScreen made

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -23,15 +23,54 @@ import {
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
+import LinearGradient from 'react-native-linear-gradient';
+
 const CategoriesScreen = ({navigation}) => {
-	const [categories, setCategories] = useState(["SALE FINAL DISCOUNTS!", "NEW IN", "CLOTHING", "SHOES", "ACTIVEWEAR", "ACCESSORIES", "TRENDING NOW", "TOPMAN", "Face + Body", "Brands", "Outlet"]);
+	const [categoriesData, setCategoriesData] = useState([
+		{name: "NEW IN", image: require('../../assets/newIn.png')},
+		{name: "CLOTHING", image: require('../../assets/clothing.png')},
+		{name: "SHOES", image: require('../../assets/shoes.png')},
+		{name: "ACTIVEWEAR", image: require('../../assets/activeWear.png')},
+		{name: "ACCESSORIES", image: require('../../assets/accessories.png')},
+		{name: "TRENDING NOW", image: require('../../assets/trendingNow.png')},
+		{name: "TOPMAN", image: require('../../assets/topMan.png')},
+		{name: "Face + Body", image: require('../../assets/faceBody.png')},
+		{name: "Brands", image: require('../../assets/brands.png')},
+		{name: "Outlet", image: require('../../assets/outlet.png')}
+	]);
 
 
 	return (
-		<View style={{flex: 1, alignItems: 'center', backgroundColor: '#FFFFFF'}}>
-			<View>
-				
+		<View style={{flex: 1, backgroundColor: '#FFFFFF', marginHorizontal: screenWidth * 0.05}}>
+			<ScrollView>
+			<TouchableOpacity>
+			<View style={{marginTop: screenHeight * 0.025}}>
+				<LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#914bf9', '#9de5e2']} style={{paddingLeft: screenWidth * 0.05, paddingTop: screenHeight * 0.05}}>
+				<View style={{justifyContent: 'flex-end'}}>
+					<Text style={{fontWeight: 'bold'}}>SALE</Text>				
+				</View>
+				</LinearGradient >
+				<LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#ffb1e5', '#914bf9']} style={{paddingLeft: screenWidth * 0.05, paddingBottom: screenHeight * 0.05}}>
+				<View style={{justifyContent: 'flex-end'}}>
+					<Text>FINAL DISCOUNTS!</Text>				
+				</View>
+				</LinearGradient >
 			</View>
+			</TouchableOpacity>
+				{categoriesData.map((categoryData) =>
+					<TouchableOpacity key={categoryData.name}>
+					<View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: screenHeight * 0.025, paddingLeft: screenWidth * 0.05, height: screenHeight * 0.20, backgroundColor: '#efefef'}}>
+						<Text style={{fontWeight: 'bold', color: '#2d2d2d'}}>{categoryData.name}</Text>
+						<Image
+							source={categoryData.image}
+							resizeMode='contain'
+							style={{width: 150, height: 150}}
+						/>
+					</View>
+					</TouchableOpacity>
+				)
+				}	
+			</ScrollView>
 		</View>
 	);
 };
--- src/screens/NewInScreen.js
+++ src/screens/NewInScreen.js
@@ -24,30 +24,79 @@ const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
 const TemplateScreen = ({navigation}) => {
-	const [currency, setCurrency] = useState('USD');
+	const [newProductsCategoriesData, setNewProductsCategoriesData] = useState([
+		{name: 'Clothing', image: require('../../assets/clothingRound.jpg')},
+		{name: 'Shoes', image: require('../../assets/shoesRound.jpg')},
+		{name: 'Accessories', image: require('../../assets/accessoriesRound.jpg')},
+		{name: 'Face + Body', image: require('../../assets/faceBodyRound.jpg')},
+		{name: 'Topman', image: require('../../assets/topManRound.jpg')},
+		{name: 'Back in stock', image: require('../../assets/backInStockRound.jpg')},
+		{name: 'Trending now', image: require('../../assets/trendingNowRound.jpg')},
+	]);
 
-	useEffect(() => {
-		const unsubscribe = navigation.addListener('focus', () => {
-			getData();
-		});
-
-		return unsubscribe;
-	}, [navigation]);
-
-	const getData = async () => {  
-		try {    
-			let jsonValue = await AsyncStorage.getItem('countryData')    
-			jsonValue = jsonValue != null ? JSON.parse(jsonValue) : null;
-			if (jsonValue != null) {
-				setCountryData(jsonValue);
-			}
-		} catch(e) {    
-			console.log(e);
-		}
-	}
+	const [newEditsData, setEditsData] = useState([
+		{name: 'EVENT ESSENTIALS', image: require('../../assets/eventEssentials.jpg')},
+		{name: 'DATE NIGHT', image: require('../../assets/dateNight.jpg')},
+		{name: 'ADIDAS', image: require('../../assets/adidas.jpg')},
+	]);
 
 	return (
-		<View style={{flex: 1, alignItems: 'center', backgroundColor: '#FFFFFF'}}>
+		<View style={{flex: 1, backgroundColor: '#eeeeee'}}>
+			<ScrollView>
+			<View style={{backgroundColor: '#ffffff', marginTop: screenHeight * 0.025, paddingBottom: 15}}>
+				<View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: screenWidth * 0.05, marginTop: screenHeight * 0.05}}>
+					<View style={{justifyContent: 'center'}}>
+					<Text style={{fontWeight: 'bold', fontSize:25, color: '#2d2d2d'}}>515</Text>
+					<Text style={{fontWeight: 'bold', fontSize: 15, color: '#2d2d2d'}}>NEW ITEMS{'\n'}IN THE LAST 24 HOURS</Text> 
+					</View>
+					<Image
+						source={require('../../assets/completo.jpg')}
+						resizeMode='cover'
+						style={{width: 75, height: 75, borderRadius: 75 / 2}}
+					/>
+				</View>
+				<TouchableOpacity style={{backgroundColor: '#2d2d2d', marginHorizontal: screenWidth * 0.05, alignItems: 'center', marginTop: screenHeight * 0.025, padding: 10}}>
+					<Text style={{color: '#ffffff', fontWeight: 'bold'}}>VIEW ALL</Text>
+				</TouchableOpacity>
+			</View>
+			<View>
+				<View style={{marginTop: screenHeight * 0.05, backgroundColor: '#eeeeee', justifyContent: 'flex-end', padding: 15}}>
+					<Text style={{fontWeight: 'bold', color: '#2d2d2d', fontSize: 15}}>NEW PRODUCTS</Text>
+				</View>
+				{newProductsCategoriesData.map((newProductCategoryData) =>
+				<>
+				<View key={newProductCategoryData.name} style={{flexDirection: 'row', backgroundColor: '#ffffff', alignItems: 'center', paddingVertical: 10}}>
+					<Image
+						source={newProductCategoryData.image}
+						resizeMode='cover'
+						style={{width: 45, height: 45, borderRadius: 45 / 2, marginLeft: screenWidth * 0.05}}
+					/>
+					<Text style={{marginLeft: 10, color: '#2d2d2d'}}>{newProductCategoryData.name}</Text>
+				</View>
+				<View style={{backgroundColor: '#eeeeee', height: 1, width: screenWidth, alignSelf: 'center'}}/>
+				</>
+				)
+				}	
+			</View>
+			<View>
+				<View style={{marginTop: screenHeight * 0.05, backgroundColor: '#eeeeee', justifyContent: 'flex-end', padding: 15}}>
+					<Text style={{fontWeight: 'bold', color: '#2d2d2d', fontSize: 15}}>NEW EDITS</Text>
+				</View>
+				<View style={{backgroundColor: '#ffffff'}}>
+				{newEditsData.map((newEditData) =>
+				<View key={newEditData.name} style={{flexDirection: 'row', backgroundColor: '#eeeeee', alignItems: 'center', justifyContent: 'space-between', paddingVertical: 30, marginHorizontal: screenWidth * 0.05, marginTop: screenHeight * 0.020}}>
+					<Text style={{color: '#2d2d2d', marginLeft: screenWidth * 0.05, fontWeight: 'bold'}}>{newEditData.name}</Text>
+					<Image
+						source={newEditData.image}
+						resizeMode='cover'
+						style={{width: 70, height: 70, borderRadius: 70 / 2, marginRight: screenWidth * 0.05}}
+					/>
+				</View>
+				)
+				}
+				</View>
+			</View>
+			</ScrollView>
 		</View>
 	);
 };
